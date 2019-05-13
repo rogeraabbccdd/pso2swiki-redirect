69e406b320c3801f5be484eb38d7b97ce0beb732
Kento <rogeraabbccdd@users.noreply.github.com>
2019-05-14 01:35:43 +0800

    Add files via upload

---
+++ pso2swiki-redirect.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        PSO2 swiki redirect.
+// @description Redirect PSO2 swiki to mirror site in google search for oversea users.
+// @include     https://www.google.com/search?*
+// @namespace   https://github.com/rogeraabbccdd/pso2swiki-redirect
+// @updateURL   https://raw.githubusercontent.com/rogeraabbccdd/pso2swiki-redirect/master/pso2swiki-redirect.user.js
+// @downloadURL https://raw.githubusercontent.com/rogeraabbccdd/pso2swiki-redirect/master/pso2swiki-redirect.user.js
+// @version     1.0
+// @author      rogeraabbccdd
+// ==/UserScript==
+let urls = document.getElementsByTagName("a");
+for(let i=0;i<urls.length;i++){
+    let link = urls[i].href;
+    let newhref = link.replace("https://pso2.swiki","https://pso2m.swiki");
+    urls[i].href = newhref
+}
