9faef167961faa87c1b97f63e0b849d61f2cc378
Kento <rogeraabbccdd@users.noreply.github.com>
2019-05-14 14:45:46 +0800

    Version 1.1
    Block mousedown event for firefox.

--- pso2swiki-redirect.user.js
+++ pso2swiki-redirect.user.js
@@ -5,12 +5,18 @@
 // @namespace   https://github.com/rogeraabbccdd/pso2swiki-redirect
 // @updateURL   https://raw.githubusercontent.com/rogeraabbccdd/pso2swiki-redirect/master/pso2swiki-redirect.user.js
 // @downloadURL https://raw.githubusercontent.com/rogeraabbccdd/pso2swiki-redirect/master/pso2swiki-redirect.user.js
-// @version     1.0
+// @version     1.1
 // @author      rogeraabbccdd
 // ==/UserScript==
 let urls = document.getElementsByTagName("a");
 for(let i=0;i<urls.length;i++){
     let link = urls[i].href;
-    let newhref = link.replace("https://pso2.swiki","https://pso2m.swiki");
-    urls[i].href = newhref
+    if(link.includes("https://pso2.swiki")){
+        let newhref = link.replace("https://pso2.swiki","https://pso2m.swiki");
+        urls[i].href = newhref;
+        // block mousedown event for firefox
+        urls[i].onmousedown = function(e){
+            return false;
+        }
+    }
 }
