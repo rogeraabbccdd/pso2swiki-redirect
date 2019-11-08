cefbb5a1cdea1521ad27bd195ea5889b5285b8ca
Kento <rogeraabbccdd@gmail.com>
2019-11-08 19:25:49 +0800

    Version 1.2
    - Support all websites.

--- pso2swiki-redirect.user.js
+++ pso2swiki-redirect.user.js
@@ -1,21 +1,21 @@
 // ==UserScript==
 // @name        PSO2 swiki redirect.
 // @description Redirect PSO2 swiki to mirror site in google search for oversea users.
-// @include     https://www.google.com/search?*
+// @include     *
 // @namespace   https://github.com/rogeraabbccdd/pso2swiki-redirect
 // @updateURL   https://raw.githubusercontent.com/rogeraabbccdd/pso2swiki-redirect/master/pso2swiki-redirect.user.js
 // @downloadURL https://raw.githubusercontent.com/rogeraabbccdd/pso2swiki-redirect/master/pso2swiki-redirect.user.js
-// @version     1.1
+// @version     1.2
 // @author      rogeraabbccdd
 // ==/UserScript==
 let urls = document.getElementsByTagName("a");
-for(let i=0;i<urls.length;i++){
-    let link = urls[i].href;
-    if(link.includes("https://pso2.swiki")){
-        let newhref = link.replace("https://pso2.swiki","https://pso2m.swiki");
-        urls[i].href = newhref;
+for(let url of urls){
+    let link = url.href;
+    if(link.includes("pso2.swiki")){
+        let newhref = link.replace("pso2.swiki","pso2m.swiki");
+        url.href = newhref;
         // block mousedown event for firefox
-        urls[i].onmousedown = function(e){
+        url.onmousedown = function(e){
             return false;
         }
     }
